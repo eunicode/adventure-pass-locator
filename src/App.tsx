cb2d925ec19d9b3021c8685a387470c6a3f31196
Eunice Park <28845173+eunicode@users.noreply.github.com>
2021-07-21 16:28:01 -0700

    Install and use normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,24 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "normalize.css";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <header>Adventure Pass Vendor Locator</header>
+      <form>
+        <label htmlFor=""></label>
+        <input
+          type="text"
+          name=""
+          value={1}
+          onChange={() => console.log("hi")}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
